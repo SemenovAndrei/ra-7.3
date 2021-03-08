e186b2985ae2854eed3ec78f8bb77d9b60f5f6e3
Andrey Semenov <andss82@gmail.com>
2021-03-08 05:52:37 +0300

    add templates

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+
+// TODO:
+// 1. Загрузите данные с помощью fetch: https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/hoc/aggregation/data/data.json
+// 2. Не забудьте вынести URL в переменные окружения (не хардкодьте их здесь)
+// 3. Положите их в state
+
+// export default class App extends React.Component {
+//   state = {
+//       list: []
+//   };
+
+//   render() {
+//       const {list} = this.state;
+//       return (
+//           <div id="app">
+//               <MonthTable list={list} />
+//               <YearTable list={list} />
+//               <SortTable list={list} />
+//           </div>
+//       );
+//   }
+// }
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" id="app">
+      <MonthTable list={list} />
+      <YearTable list={list} />
+      <SortTable list={list} />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
