e772e58ba35bc0532acc222a8f64d09de57572df
Andrey Semenov <andss82@gmail.com>
2021-03-08 06:06:27 +0300

    fetch data done

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
+import { useEffect, useState } from 'react'
 import './App.css'
+import MonthTable from './components/MonthTable/MonthTable'
+import SortTable from './components/SortTable/SortTable'
+import YearTable from './components/YearTable/YearTable'
 
 // TODO:
 // 1. Загрузите данные с помощью fetch: https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/hoc/aggregation/data/data.json
@@ -23,6 +27,18 @@ import './App.css'
 // }
 
 function App() {
+  const [list, setList] = useState([])
+
+  useEffect(() => {
+    const loadData = async () => {
+      const response = await fetch(process.env.REACT_APP_URL_DATA)
+      console.log(response)
+      const json = await response.json()
+      setList(json)
+    }
+    loadData()
+  }, [])
+
   return (
     <div className="App" id="app">
       <MonthTable list={list} />
